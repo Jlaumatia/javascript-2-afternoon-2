76d294747507417c96826d2454d491b52dd594c8
Cameron Laumatia <Jlaumatia>
2020-02-17 17:19:43 -0700

    completed

--- practice.js
+++ practice.js
@@ -18,6 +18,10 @@ var arr = [10,20,30];
 */
 
 //Code Here
+function first(arr){
+  return arr[0]
+}
+first(arr)
 
 
 
@@ -33,6 +37,10 @@ var arr = [40,50,60];
 */
 
 //Code Here
+function last(arr){
+  return arr[2];
+}
+last(arr)
 
 
 
@@ -48,6 +56,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
+function looper(family){
+  for (var i = 0; i < family.length; i++){
+    alert(family[i])
+  }
+
+  looper(family)
 
 
 
@@ -63,6 +77,13 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper(letters){
+  for (let i = letters.length - 1; i >= 0; i--){
+  alert(letters[i]);
+}
+}
+reversedLooper(letters)
+  
 
 
 
@@ -78,6 +99,19 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+
+function evenFinder(nums){
+  for (var i = 0; i < nums.length; i++){
+    if(nums[i] % 2 !== 0){
+      delete nums[i];
+    }
+  }
+  return nums
+
+}
+evenFinder(nums)
+  
+
 
 
 
